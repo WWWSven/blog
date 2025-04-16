f5ea0e0b7ca87b0183bf264c8f97c19647197bfa
Sven <1981907427@qq.xom>
2025-04-16 08:25:56 +0800

    init

---
+++ app/(home)/page.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+export const metadata = {
+  title: 'My App',
+};
+
+export default function HomePage() {
+  return (
+    <main
+      style={{
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        textAlign: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <h1
+        style={{
+          fontSize: '2rem',
+          fontWeight: 'bold',
+          marginBottom: '1rem',
+        }}
+      >
+        Sven
+      </h1>
+    </main>
+  );
+}
